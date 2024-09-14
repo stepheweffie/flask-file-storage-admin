43f23a5d45b406ab991e919010db842875e4d51b
stepheweffie <stephie.maths@gmail.com>
2024-09-14 06:33:45 -0400

    incoming commits

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, send_from_directory, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_babel import Babel
+# from flask_meld import Meld
 from flask_admin.contrib.fileadmin import FileAdmin
 from flask_bootstrap import Bootstrap5
 from views import HomeView
 from forms import DropzoneForm, CKTextAreaField
+from oryx_api import create_srs_admin_blueprint
 from models import db, Upload
 from plugins import Plugins 
 from werkzeug.middleware.proxy_fix import ProxyFix
 from utils import build_sample_db, handle_form_data
+
 # from config import Config
 import os.path as op
 import os
 
 bs = Bootstrap5()
 babel = Babel()
+# meld = Meld()
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -30,8 +34,8 @@ def create_app():
             pass
             
     # app.config.from_object(config_class)
-    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = os.environ.get('BST')
-    app.config['SECRET_KEY'] = 'SECRET_KEY'
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'
+    app.config['SECRET_KEY'] = '134975049820347'
     app.config['DATABASE_FILE'] = 'admin.sqlite'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
     app.config['SQLALCHEMY_ECHO'] = True
@@ -39,15 +43,29 @@ def create_app():
     app.config['UPLOAD_PATH'] = op.join(op.dirname(__file__), 'uploads')
     app.config['VIDEO_PATH'] = op.join(op.dirname(__file__), 'files/videos')
     app.config['APP_USERNAME'] = os.environ.get('APP_USERNAME')
+
+    app.config['SRS_API_URL'] = 'http://live.savantlab.org:1985'
+    app.config['SRS_USERNAME'] = 'admin'
+    app.config['SRS_PASSWORD'] = 'admin'
+
+    # Create and register blueprints
+    oryx_api = create_srs_admin_blueprint(
+        app.config['SRS_API_URL'],
+        app.config['SRS_USERNAME'],
+        app.config['SRS_PASSWORD']
+    )
+    
+    app.register_blueprint(oryx_api)
     
     # Initialize extensions
     db.init_app(app)
     bs.init_app(app)
     babel.init_app(app)
+    # meld.init_app(app)
     Plugins.load_plugins()
     
     # Apply ProxyFix middleware for proper handling of proxy headers
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+    # app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     # Add admin views
     from views import HomeView
